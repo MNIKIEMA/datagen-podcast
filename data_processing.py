d61c8ce31973b4eecfb66595f46770401715353f
Mahamadi NIKIEMA <nikiemamahamadi01@gmail.com>
2023-04-29 19:28:03 +0200

    add processing function

---
+++ data_processing.py
@@ -0,0 +1,39 @@
+from tqdm.auto import tqdm
+import json
+
+with open("./data/episode.json", encoding="utf-8") as f:
+    episode_info = json.load(f)
+
+with open("./data/transcription.json", encoding="utf-8") as f:
+    transcription = json.load(f)
+
+
+def process(transcription, episode_info):
+
+    new_data = []
+
+    window = 6  # number of sentences to combine
+    stride = 3  # number of sentences to 'stride' over, used to create overlap
+    # print(episode_info[0].keys())
+    for episode_id, data in tqdm(transcription.items()):
+        data = data["chunks"]
+        episode_data = [ep_info for ep_info in episode_info if ep_info["name"].startswith("#"+episode_id[11:])][0]
+        print(episode_data["name"], episode_id)
+        for i in range(0, len(data), stride):
+            i_end = min(len(data)-1, i+window)
+            
+            text = " ".join([text['text'] for text in data[i:i_end]])
+            #print("*"*100)
+            #print("+++++", text)
+            new_data.append({
+                'start':  data[i]['timestamp'][0],
+                'end': data[i_end]['timestamp'][1],
+                'title': episode_data["name"],
+                'text': text,
+                'id': episode_data["id"],
+                'url': episode_data["external_urls"]["spotify"],
+                'published': episode_data["release_date"]
+            })
+
+
+process(transcription, episode_info)
